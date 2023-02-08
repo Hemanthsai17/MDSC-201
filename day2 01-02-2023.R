2007ef0e92b8fd6ea26364167d4d8ab70e986bf3
Hemanth Sai Pavan Immidisetty <64628460+Hemanthsai17@users.noreply.github.com>
2023-02-08 15:06:35 +0530

    Add files via upload

---
+++ day2 01-02-2023.R
@@ -0,0 +1,15 @@
+arr= array(c('sai','pavan','hemanth'),dim=c(3,1,2))
+print(arr)
+
+apple_colors <- c('green','green','yellow','red','red','red','green')
+factor_apple <- factor(apple_colors)
+print(factor_apple)
+print(nlevels(factor_apple))
+
+BMI <- data.frame(
+  gender = c("Male", "Male","Female"),
+  height = c(162, 171.5, 165), 
+  weight = c(84,73, 78), 
+  Age =c(45,39,36) ) 
+
+print(BMI)
