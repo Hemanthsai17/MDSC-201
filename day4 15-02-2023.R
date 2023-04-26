d0141b336af08ee56d6c44310849a45df0befa1c
Hemanth Sai Pavan Immidisetty <64628460+Hemanthsai17@users.noreply.github.com>
2023-04-26 12:23:32 +0530

    Add files via upload

---
+++ day4 15-02-2023.R
@@ -0,0 +1,15 @@
+# Examples of inbuilt functions
+
+# Creating a sequence of numbers
+print(seq(1,11))
+# For finding mean of numbers between an interval
+print(mean(32:82))
+# For finding sum of numbers between an interval
+print(sum(1:20))
+# For finding square root of a number
+print(sqrt(49))
+# For finding floor of a real number
+print(floor(45.68))
+# For finding ceiling of a real number
+print(ceiling(45.68))
+
